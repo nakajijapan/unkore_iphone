6ff8fc6ad3962595e066833d5612b18d8baad65e
nakajijapan <nakajima@paperboy.co.jp>
2012-08-01 21:16:01 +0900

    1.3秒ごとにランダム（重み付きあり）で敵を発生

--- unkore/EnemyCache.m
+++ unkore/EnemyCache.m
@@ -17,6 +17,12 @@
 
 @implementation EnemyCache
 
+// 敵発生用配列
+static int      totalSpawnSize;
+static CCArray* spawnFrequency;
+static CCArray* spawnFrequencyInfo;
+
+
 +(id) cache
 {
 	return [[[self alloc] init] autorelease];
@@ -26,6 +32,7 @@
 {
 	if ((self = [super init]))
 	{
+       
 		// get any image from the Texture Atlas we're using
         // テクスチャアトラスからいずれかの画像を取得する
 		CCSpriteFrame* frame = [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:@"game_safe001.png"];
@@ -50,42 +57,56 @@
 	// create the enemies array containing further arrays for each type
 	enemies = [[CCArray alloc] initWithCapacity:EnemyType_MAX];
 	
+    totalSpawnSize = 0;
+    spawnFrequency = [[CCArray alloc] initWithCapacity:EnemyType_MAX];
+    
 	// create the arrays for each type
 	for (int i = 0; i < EnemyType_MAX; i++)
 	{
 		// depending on enemy type the array capacity is set to hold the desired number of enemies
 		int capacity;
+        int fruquency;
 		switch (i)
 		{
 			case EnemyTypeSafe001:
-				capacity = 100;
+				capacity = 30;
+                fruquency = 100;
 				break;
 			case EnemyTypeSafe002:
 				capacity = 30;
+                fruquency = 70;
 				break;
 			case EnemyTypeSafe003:
 				capacity = 30;
+                fruquency = 70;
 				break;
 			case EnemyTypeSafe004:
 				capacity = 30;
+                fruquency = 70;
 				break;
 			case EnemyTypeSafe005:
 				capacity = 10;
+                fruquency = 70;
 				break;                
 			case EnemyTypeSafe006:
 				capacity = 10;
+                fruquency = 70;
 				break; 
 			case EnemyTypeOut001:
 				capacity = 30;
+                fruquency = 70;
 				break;
 			case EnemyTypeOut002:
 				capacity = 30;
+                fruquency = 70;
 				break;
 			case EnemyTypeOut003:
 				capacity = 20;
+                fruquency = 70;
 				break;
 			case EnemyTypeOut004:
 				capacity = 10;
+                fruquency = 70;
 				break;
 
 			default:
@@ -93,11 +114,28 @@
 				break;
 		}
 		
+        // 敵毎の重みを設定（低いものほど発生しやすい）
+        [spawnFrequency insertObject:[NSNumber numberWithInt:fruquency] atIndex: i];
+        totalSpawnSize += fruquency;
+        
 		// no alloc needed since the enemies array will retain anything added to it
 		CCArray* enemiesOfType = [CCArray arrayWithCapacity:capacity];
 		[enemies addObject:enemiesOfType];
 	}
-	
+    
+    // 重み情報初期化
+    //[spawnFrequencyInfo initWithCapacity:totalSpawnSize];
+    spawnFrequencyInfo = [[CCArray alloc] initWithCapacity:totalSpawnSize];
+    for (int i = 0; i < EnemyType_MAX; i++) {
+        int enemyCount = [[spawnFrequency objectAtIndex:i] intValue];
+        CCLOG(@"enemy %d : %d", i, enemyCount);
+        for (int j = 0; j < enemyCount; j++) {
+            [spawnFrequencyInfo addObject:[NSNumber numberWithInt:i]];
+        }
+    }
+    
+    
+	CCLOG(@"enemyCacheSzie %d", totalSpawnSize);
     CCLOG(@"enemy max = %d", EnemyType_MAX);
     
     // 敵のキャッシュ情報を作成する
@@ -108,8 +146,10 @@
 		int numEnemiesOfType = [enemiesOfType capacity];
 		
 		for (int j = 0; j < numEnemiesOfType; j++) {
+
             // 敵生成
 			EnemyEntity* enemy = [EnemyEntity enemyWithType:i];
+
             // batchに追加
 			[batch addChild:enemy z:0 tag:i];
             
@@ -140,37 +180,30 @@
 		}
 	}
 }
-/*
- -(void) checkForBulletCollisions
- {
- EnemyEntity* enemy;
- CCARRAY_FOREACH([batch children], enemy)
- {
- if (enemy.visible)
- {
- BulletCache* bulletCache = [[GameScene sharedGameScene] bulletCache];
- CGRect bbox = [enemy boundingBox];
- if ([bulletCache isPlayerBulletCollidingWithRect:bbox])
- {
- // This enemy got hit ...
- [enemy gotHit];
- }
- }
- }
- }
- */
 
 
-
-
+// スケジューラ更新
 -(void) scheduleUpdate
 {
-    [self schedule:@selector(generateEnemy:) interval:.1f];
+    [self schedule:@selector(generateEnemy:) interval:1.3f];
+    //[self schedule:@selector(generateEnemy:) interval:.1f];
 }
+
+
 // 敵を発生させる
 -(void) generateEnemy: (id)selector
 {
+    int maxSize = spawnFrequencyInfo.count;
+    int enemyTypeOffset = (int)(CCRANDOM_0_1() * maxSize) % maxSize ;
+    //CCLOG(@"show rand -----------------%d -> enemy = %d", enemyTypeOffset, [[spawnFrequencyInfo objectAtIndex:enemyTypeOffset] intValue]);
+    
+    // 重みに応じて敵を発生させる（あまりが0のときに敵発生）
+    updateCount++;
+    [self spawnEnemyOfType: [[spawnFrequencyInfo objectAtIndex:enemyTypeOffset] intValue]];
+    
+    
     // 敵の種類分操作
+    /*
 	updateCount++;
 	for (int i = EnemyType_MAX - 1; i >= 0; i--) {
         
@@ -184,6 +217,7 @@
 			break;
 		}
 	}
+     */
     
 }
 
