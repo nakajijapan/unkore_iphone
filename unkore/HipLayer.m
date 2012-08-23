b402310baf88fa9ce3dcb68505dc53b6156a3888
nakajijapan <nakajima@paperboy.co.jp>
2012-08-24 00:07:40 +0900

    不要コメント削除

--- unkore/HipLayer.m
+++ unkore/HipLayer.m
@@ -20,18 +20,7 @@
 		uiframe.anchorPoint = CGPointMake(0, 1);
 		[self addChild:uiframe z:0 tag:UILayerTagFrameSprite];
 		
-		// Fake User Interface which does nothing.
-        /*
-		CCLabelTTF* label = [CCLabelTTF labelWithString:@"Hip" fontName:@"Courier" fontSize:22];
-		label.color = ccBLACK;
-		label.position = CGPointMake(screenSize.width / 2, screenSize.height);
-		label.anchorPoint = CGPointMake(0.5f, 1);
-		[self addChild:label];
-         */
-		
 		self.isTouchEnabled = YES;
-        
-        //CCRepeatForever *repeat = [CCRepeatForever actionWithAction: [CCRotateBy actionWithDuration:1.0f angle:360]];
         
         // お尻を左右に振らせる
         CCMoveBy* move1 = [CCMoveBy actionWithDuration:.1f  position: ccp(5,0)];
@@ -41,10 +30,6 @@
         CCDelayTime* delay = [CCDelayTime actionWithDuration:5.0f];
         CCSequence* sequence = [CCSequence actions:move1, move2, move3, move4, move1, move2, move3, move4, delay, nil];
         CCRepeatForever *repeat = [CCRepeatForever actionWithAction: sequence];
-        /*
-         id shaky = [CCShaky3D actionWithRange:1 shakeZ:NO grid:ccg(15,10) duration:2];
-         CCRepeatForever *repeat = [CCRepeatForever actionWithAction: shaky];
-         */
         
         [self runAction:repeat];
 	}
@@ -53,11 +38,6 @@
 
 -(void) show
 {
-    
-    //CGSize screenSize = [[CCDirector sharedDirector] winSize];
-    
-    //CCLOG(@"HipLayer show  : %f", screenSize.height);
-    
     id action1 = [CCMoveBy actionWithDuration:2.0f  position: ccp(0, -250)];
     id action2 = [CCEaseElasticOut actionWithAction:action1 period:0.35f];
     [self runAction:action2];
