f3ccc6baa14d5121c602c160db06d5884a33ca63
nakajijapan <nakajima@paperboy.co.jp>
2013-01-09 03:40:12 +0900

    add DeviceUtil

---
+++ unkore/DeviceUtil.h
@@ -0,0 +1,22 @@
+//
+//  DeviceUtil.h
+//  nakajima
+//
+//  Created by Nakajima Daichi on 13/01/09.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DeviceUtil : NSObject {
+    
+}
+
+
++(int) getHeightOffset;
++(NSString *) platform;
+
+@end
+
+
+
